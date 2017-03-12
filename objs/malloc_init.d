20fc4d68e43dba940796118bef2f95617488e551
Antoine Riard <ariard@student.42.fr>
2017-03-12 01:36:50 +0100

    bin ok, linking needed

--- objs/malloc_init.d
+++
@@ -1,7 +0,0 @@
-objs/malloc_init.o: src/malloc_init.c includes/malloc.h includes/types.h \
-  includes/../libft/includes/libft.h includes/../libft/includes/lst.h \
-  includes/../libft/includes/dlst.h includes/../libft/includes/str.h \
-  includes/../libft/includes/sstr.h includes/../libft/includes/math.h \
-  includes/../libft/includes/btree.h includes/../libft/includes/mytime.h \
-  includes/../libft/includes/get_next_line.h \
-  includes/../libft/includes/color.h includes/../libft/includes/ft_sys.h
