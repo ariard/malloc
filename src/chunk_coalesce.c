5a11cb47abda5d6e2a5b46d5998927df2d4c96dc
Antoine Riard <ariard@student.42.fr>
2018-01-30 18:33:44 +0100

    [bug] coalesce + merge forward ok

--- src/chunk_coalesce.c
+++ src/chunk_coalesce.c
@@ -6,7 +6,7 @@
 /*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/07 20:32:17 by ariard            #+#    #+#             */
-/*   Updated: 2018/01/30 00:15:32 by ariard           ###   ########.fr       */
+/*   Updated: 2018/01/30 18:32:32 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void		*chunk_coalesce(t_area *ar, t_chunk *free, size_t req, char range)
 
 	bs = chunk_find(ar, (void *)free);
 	req = (bs.a == 0) ? align(req, 16) : align(req, 512);
-	write(3, "co - flag A\n", 12);
 	while (bs.bin && bs.a < 2 && free)
 	{
 		cand = (t_cand) { free, 0, 0, 0 };
@@ -34,7 +33,7 @@ void		*chunk_coalesce(t_area *ar, t_chunk *free, size_t req, char range)
 		free = (range == 0) ? free->next : NULL;
 		if (cand.size > req)
 			return (chunk_merge(bs.bin, cand.chunk, cand.forward
-				+ BT(cand.chunk), cand.backward));
+				+ (BT(cand.chunk) & ~(1 << 0)), cand.backward, bs.a));
 	}
 	return (NULL);
 }
--- src/chunk_merge.c
+++ src/chunk_merge.c
@@ -6,7 +6,7 @@
 /*   By: ariard <ariard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 19:33:14 by ariard            #+#    #+#             */
-/*   Updated: 2018/01/30 00:19:57 by ariard           ###   ########.fr       */
+/*   Updated: 2018/01/30 18:33:08 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,42 +18,53 @@ static void	clean_chunk(t_chunk *tmp)
 
 	if (tmp->next && (tmp->next)->prev)
 		(tmp->next)->prev = tmp->prev;
-	write(3, "cm - flag 1\n", 12);
 	if (tmp->prev && (tmp->prev)->next)
 		(tmp->prev)->next = tmp->next;
 	tmp->next = NULL;
 	tmp->prev = NULL;
-	write(3, "cm - flag 2\n", 12);
 	size = BT(tmp) & ~(1 << 0);
-	print_value(3, size);
-	write(3, "\n", 1);
+//	print_value(3, size);
+//	write(3, "\n", 1);
+//	print_addr(3, (unsigned long)tmp);
+//	write(3, "\n", 1);
+//	print_addr(3, (unsigned long)LSUM_ADDR((void *)tmp, size));
+//	write(3, "\n", 1);
 	SUM(tmp) = 0;
-	write(3, "cm - flag 3\n", 12);
-	LSUM(tmp, size) = 0;
-	write(3, "cm - flag 4\n", 12);
+//	write(3, "cm - flag 3\n", 12);
+	LSUM((void *)tmp, size) = 0;
+//	write(3, "cm - flag 4\n", 12);
 }
 
 void		*chunk_merge(t_bin *bin, 
-		t_chunk *chunk, size_t forward, size_t backward)
+		t_chunk *chunk, size_t forward, size_t backward, char a)
 {
 	t_chunk		*tmp;
 	size_t		new_size;
+	size_t		s_bin;
 
 	new_size = forward + backward;
 	tmp = chunk;
+	s_bin = (a == 0) ? g_cfg.tiny_area : g_cfg.small_area;
+//	write(3, "max of bin :", 12);
+//	print_value(3, s_bin);
+//	write(3, "\n", 1);
+//	write(3, "bin : ", 6);
+//	print_addr(3, (unsigned long)bin);
+//	write(3, "\n", 1);
+//	write(3, "bin end : ", 10);
+//	print_addr(3, (unsigned long)(char *)bin + s_bin);
+//	write(3, "\n", 1);
+//	write(3, "forward :", 9);
+//	print_value(3, forward);
+//	write(3, "\n", 1);
 	while (forward)
 	{
-		write(3, "cm - flag 0\n", 12);
 		clean_chunk(tmp);
-		write(3, "cm - flag A\n", 12);
 		if ((forward -= (BT(tmp) & ~(1 << 0))) == 0)
 			break;
 		tmp = ((void *)tmp + (BT(tmp) & ~(1 << 0)));
-		write(3, "cm - flag B\n", 12);
-		bin->nb--;	
-		write(3, "cm - flag C\n", 12);
+		bin->nb--;
 	}
-	write(3, "cm - flag D\n", 12);
 	tmp = chunk;
 	while (backward)
 	{
